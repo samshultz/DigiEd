688d0c8c0bc6e629140b75c6a8398d59ef18696e
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-01 20:21:18 +0100

    change connection max age

--- myshop/settings/pro.py
+++ myshop/settings/pro.py
@@ -57,4 +57,4 @@ import dj_database_url
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+DATABASES['default']['CONN_MAX_AGE'] = 1500
