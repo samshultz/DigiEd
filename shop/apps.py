8486493c4f6cd9fdf094d2f000d150cbd794b786
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-18 04:45:07 +0100

    changed some setting

--- shop/apps.py
+++ shop/apps.py
@@ -6,5 +6,5 @@ class ShopConfig(AppConfig):
     name = 'shop'
 
     def ready(self):
-        elasticsearch_dsl.connections.connections.create_connection(hosts=['https://ef45e787737c2385b89f93820c9bfea6.us-east-1.aws.found.io'], timeout=20)
+        elasticsearch_dsl.connections.connections.create_connection(hosts=['https://ef45e787737c2385b89f93820c9bfea6.us-east-1.aws.found.io'], http_auth="elastic:veOFdNEXM0ugmxJsgauaKrH1", timeout=20)
         import shop.signals
