97973547924515b5a6d02fec1886eb233247bf84
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-19 12:52:42 +0100

    updated requirements.txt

--- orders/tasks.py
+++ orders/tasks.py
@@ -11,12 +11,12 @@ def order_created(order_id, tx_ref):
     successfully created.
     """
     order = Order.objects.get(id=order_id, tx_ref=tx_ref)
-    download_url = "http://127.0.0.1:8000{}".format(reverse_lazy(
+    download_url = "http://dlearn.tk{}".format(reverse_lazy(
         "orders:order_detail", kwargs={'order_id': order_id, 'tx_ref': tx_ref}))
     subject = 'Order nr. {}'.format(order.id)
     message = 'Dear {},\n\nYou have successfully placed an order.\
     Your order id is {}.\n\nClick on the link below to download \
     your resource. \n\n {}'.format(order.first_name, order.id, download_url)
-    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    mail_sent = send_mail(subject, message, 'noreply@dlearn.tk', [order.email])
 
     return mail_sent
