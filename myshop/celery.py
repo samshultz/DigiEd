f97debf2ac70ec04a01fd02f79a120dbd45ffc37
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-21 19:49:32 +0100

    changed settings for celery

--- myshop/celery.py
+++ myshop/celery.py
@@ -4,7 +4,8 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+set = os.environ.get("DJANGO_SETTINGS_MODULE", "myshop.settings.local")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', set)
 app = Celery('myshop')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
