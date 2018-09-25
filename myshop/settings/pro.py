e70e36de6c858231990a81a2a449e6c5c1bae8c8
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-25 17:14:11 +0100

    updated whitenoise settings

--- myshop/settings/pro.py
+++ myshop/settings/pro.py
@@ -64,3 +64,5 @@ HTTP_AUTH = os.environ.get("HTTP_AUTH", "elastic:veOFdNEXM0ugmxJsgauaKrH1")
 CELERY_BROKER_URL = os.environ.get("CLOUDAMQP_URL")
 
 CELERY_BROKER_POOL_LIMIT = 3
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
