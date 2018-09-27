f7bc124f6158f510911a1e6e197c2cad873072a4
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-27 15:17:52 +0100

    removed conditional from signals

--- shop/signals.py
+++ shop/signals.py
@@ -5,32 +5,30 @@ from shop.es_docs import ESBook
 
 @receiver(post_save, sender=Book)
 def index_data(sender, instance, created, **kwargs):
-    if created:
-        if instance.image:
-                image = instance.image.url
-        else:
-            image = instance.image_url
+    
+    if instance.image:
+            image = instance.image.url
+    else:
+        image = instance.image_url
+    
+    if instance.book_file:
+        download_link = instance.book_file.url
+    else:
+        download_link = instance.book_url
+
+    esp = ESBook(meta={'id': instance.pk},
+                 title=instance.name,
+                 image=image,
+                 url=instance.get_absolute_url(),
+                 price=instance.price,
+                 publisher=instance.publisher,
+                 author=instance.author,
+                 download_link=download_link
+
+                )
         
-        if instance.book_file:
-            download_link = instance.book_file.url
-        else:
-            download_link = instance.book_url
-
-        esp = ESBook(meta={'id': instance.pk},
-                        title=instance.name,
-                        image=image,
-                        url=instance.get_absolute_url(),
-                        price=instance.price,
-                        publisher=instance.publisher,
-                        author=instance.author,
-                        download_link=download_link
-
-                        )
-            
-            
-        esp.save(index='books')
+    esp.save(index='books')
 
 @receiver(post_delete, sender=Book)
 def remove_book_from_index(sender, instance, **kwargs):
     ESBook().get(id=instance.pk, index='books').delete()
-    
