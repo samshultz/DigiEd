460cda6e04fb6067bd728ef7fb74082f19028be2
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-09-28 16:20:34 +0100

    changed how new collections are displayed

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -11,7 +11,7 @@ def show_new_books(count=5):
 
 @register.inclusion_tag('templatetagshtml/new_releases.html')
 def show_new_releases(count=4):
-    new_rel = Book.objects.order_by('-year')[:count]
+    new_rel = Book.objects.order_by('-created')[:count]
     cart_product_form = CartAddProductForm(initial={'quantity': "1",
                                                     'update': False})
     return {'new_releases': new_rel, 'cart_product_form': cart_product_form}
